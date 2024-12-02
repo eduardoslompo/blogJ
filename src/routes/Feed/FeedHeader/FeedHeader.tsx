c9f507593d9ce63d3c3f42f2afbac6ee4e91ed0c
Eduardo Slompo <edu_slompo@hotmail.com>
2024-12-02 20:54:34 -0300

    Update FeedHeader.tsx

--- src/routes/Feed/FeedHeader/FeedHeader.tsx
+++ src/routes/Feed/FeedHeader/FeedHeader.tsx
@@ -1,10 +1,10 @@
-import { TCategories } from "src/types"
-import React from "react"
-import CategorySelect from "./CategorySelect"
-import OrderButtons from "./OrderButtons"
-import styled from "@emotion/styled"
+import { TCategories } from "src/types";
+import React from "react";
+import CategorySelect from "./CategorySelect";
+import OrderButtons from "./OrderButtons";
+import styled from "@emotion/styled";
 
-type Props = {}
+type Props = {};
 
 const FeedHeader: React.FC<Props> = () => {
   return (
@@ -12,10 +12,10 @@ const FeedHeader: React.FC<Props> = () => {
       <CategorySelect />
       <OrderButtons />
     </StyledWrapper>
-  )
-}
+  );
+};
 
-export default FeedHeader
+export default FeedHeader;
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -23,4 +23,4 @@ const StyledWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray6};
-`
+`;
