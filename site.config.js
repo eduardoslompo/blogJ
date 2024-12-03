0f9c946658b2b9e4bfa544b05546457a6528faf4
Eduardo Slompo <edu_slompo@hotmail.com>
2024-12-03 15:37:18 -0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://blog-j.vercel.app",
+  link: "https://testeescola.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "pt-BR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
