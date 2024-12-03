a1d80a50f1a015f2f8244b461fb5193f0ae7863a
Eduardo Slompo <edu_slompo@hotmail.com>
2024-12-03 14:46:06 -0300

    Update ContactCard.tsx

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -1,13 +1,8 @@
-import styled from "@emotion/styled"
-import React from "react"
-import {
-  AiFillLinkedin,
-  AiOutlineGithub,
-  AiOutlineInstagram,
-  AiOutlineMail,
-} from "react-icons/ai"
-import { CONFIG } from "site.config"
-import { Emoji } from "src/components/Emoji"
+import React from 'react';
+import styled from '@emotion/styled';
+import { AiOutlineMail } from 'react-icons/ai';
+import { CONFIG } from 'site.config';
+import { Emoji } from 'src/components/Emoji';
 
 const ContactCard: React.FC = () => {
   return (
@@ -16,65 +11,31 @@ const ContactCard: React.FC = () => {
         <Emoji>💬</Emoji> Contato
       </StyledTitle>
       <StyledWrapper>
-        {CONFIG.profile.github && (
-          <a
-            href={`https://github.com/${CONFIG.profile.github}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiOutlineGithub className="icon" />
-            <div className="name">github</div>
-          </a>
-        )}
-        {CONFIG.profile.instagram && (
-          <a
-            href={`https://www.instagram.com/${CONFIG.profile.instagram}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiOutlineInstagram className="icon" />
-            <div className="name">instagram</div>
-          </a>
-        )}
         {CONFIG.profile.email && (
-          <a
-            href={`mailto:${CONFIG.profile.email}`}
-            rel="noreferrer"
-            target="_blank"
-            css={{ overflow: "hidden" }}
-          >
+          <a href={`mailto:${CONFIG.profile.email}`} rel="noreferrer" target="_blank" css={{ overflow: 'hidden' }}>
             <AiOutlineMail className="icon" />
             <div className="name">email</div>
           </a>
         )}
-        {CONFIG.profile.linkedin && (
-          <a
-            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiFillLinkedin className="icon" />
-            <div className="name">linkedin</div>
-          </a>
-        )}
       </StyledWrapper>
     </>
-  )
-}
+  );
+};
 
-export default ContactCard
+export default ContactCard;
 
 const StyledTitle = styled.div`
   padding: 0.25rem;
   margin-bottom: 0.75rem;
-`
+`;
+
 const StyledWrapper = styled.div`
   display: flex;
   padding: 0.25rem;
   flex-direction: column;
   border-radius: 1rem;
-  background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
+  background-color: ${({ theme }) => (theme.scheme === 'light' ? 'white' : theme.colors.gray4)};
+
   a {
     display: flex;
     padding: 0.75rem;
@@ -88,13 +49,15 @@ const StyledWrapper = styled.div`
       color: ${({ theme }) => theme.colors.gray12};
       background-color: ${({ theme }) => theme.colors.gray5};
     }
+
     .icon {
       font-size: 1.5rem;
       line-height: 2rem;
     }
+
     .name {
       font-size: 0.875rem;
       line-height: 1.25rem;
     }
   }
-`
+`;
